372f2902ba1386bbf5a41fbabf2801af3e9c6d71
git <jurij.acalinovic@itsl.lu>
2017-11-19 18:19:27 +0100

    First commit

--- debug/debug.sh
+++ debug/debug.sh
@@ -10,7 +10,7 @@
 #
 
 # Set start time
-STARTTIME==`date +%s`
+STARTTIME=`date +%s`
 
 # Define script path and include config and includes files
 SCRIPT_PATH=$(dirname $(readlink -f $0))
@@ -24,8 +24,30 @@ source $SCRIPT_PATH/../includes/functions.inc
 echo "========================================================="
 echo "     DEBUG MODE FOR $HOSTNAME - NOTHING WILL BE DONE"
 echo "========================================================="
-echo "date and time =>" echoNowTime
+echo "date and time =>" $(echoNowTime)
 echo "SCRIPT_PATH =>" $SCRIPT_PATH
-echo "=========================================================="
+echo ""
+echo "LOGS_FILE_PATH =>" $LOGS_FILE_PATH
+echo "TEMP_FILE_PATH =>" $TEMP_FILE_PATH
+echo "PPK_KEY_PATH =>" $PPK_KEY_PATH
+echo "EMAIL_TO_ADDRESS =>" $EMAIL_TO_ADDRESS
+echo ""
+echo "SOURCE_ADDRESS =>" $SOURCE_ADDRESS
+echo "SOURCE_USERNAME =>" $SOURCE_USERNAME
+echo "SOURCE_DATASTORE1_PATH =>" $SOURCE_DATASTORE1_PATH
+echo "SOURCE_DATASTORE2_PATH =>" $SOURCE_DATASTORE2_PATH
+echo "SOURCE_GHETTOVCB_DIR =>" $SOURCE_GHETTOVCB_DIR
+echo "SOURCE_VM_LIST =>" $SOURCE_VM_LIST
+echo ""
+echo "DESTINATION_ADDRESS =>" $DESTINATION_ADDRESS
+echo "DESTINATION_USERNAME =>" $DESTINATION_USERNAME
+echo "DESTINATION_DATASTORE1_PATH =>" $DESTINATION_DATASTORE1_PATH
+echo "DESTINATION_DATASTORE2_PATH =>" $DESTINATION_DATASTORE2_PATH
+echo ""
+
+sleep 3
+ENDTIME=`date +%s`
+echo "Duration "echoDeltaTime $STARTTIME $ENDTIME
+echo "========================================================="
 
 exit 0
