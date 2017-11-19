3becf5fe9c8a76ce6d01093e9ea24a8106fce2cd
git <jurij.acalinovic@itsl.lu>
2017-11-19 13:17:39 +0100

    First commit

--- debug/debug.sh
+++ debug/debug.sh
@@ -9,3 +9,23 @@
 # Version:           0.0.1 - 
 #
 
+# Set start time
+STARTTIME==`date +%s`
+
+# Define script path and include config and includes files
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+
+source $SCRIPT_PATH/../conf/main.conf
+source $SCRIPT_PATH/../includes/functions.inc
+
+
+# Print variables values
+
+echo "========================================================="
+echo "     DEBUG MODE FOR $HOSTNAME - NOTHING WILL BE DONE"
+echo "========================================================="
+echo "date and time =>" echoNowTime
+echo "SCRIPT_PATH =>" $SCRIPT_PATH
+echo "=========================================================="
+
+exit 0
