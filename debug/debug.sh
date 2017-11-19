e92d9ab7f57e720cd85252a6a3dd5aecd6b08892
git <jurij.acalinovic@itsl.lu>
2017-11-19 11:55:12 +0100

    First commit

---
+++ debug/debug.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# ITSL-VMBS - Virtual Machine Backups Scripts
+# Purpose:					debug and tests
+#
+# Authors: 					jurij.acalinovic@itsl.lu
+#										boris.acalinovic@gmail.com
+#
+# Version:					0.0.1 - 
+#
+
